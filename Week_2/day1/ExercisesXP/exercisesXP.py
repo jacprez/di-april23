76c2566687cbaf36594b0e763e3a581ff3958afd
jacprez <jprezant1@gmail.com>
2023-05-07 15:33:40 +0300

    week 2 day 1 xp and notes

---
+++ Week_2/day1/ExercisesXP/exercisesXP.py
@@ -0,0 +1,67 @@
+#Exercise 1 : Hello World
+
+print("Hello World\n" * 4)
+
+
+#  Exercise 2 : Some Math
+
+print(pow(99, 3) * 8)
+
+# Exercise 3 : What Is The Output ?
+
+# >>> 5 < 3       # False
+# >>> 3 == 3      # True
+# >>> 3 == "3"    # False
+# >>> "3" > 3     # Error
+# >>> "Hello" == "hello"  #False
+
+# Exercise 4 : Your Computer Brand
+
+computer_brand = "MacBook"
+print(f"I have a {computer_brand} computer")
+
+
+# Exercise 5 : Your Information
+
+name = "Jackie"
+age = 26
+shoe_size = 40
+info = f"My name is {name}. I am {26} years old and have a size {shoe_size}."
+print(info)
+
+# Exercise 6 : A & B
+
+a = 10
+b = 5
+
+if a > b :
+    print("Hello World")
+    
+# Exercise 7 : Odd Or Even 
+
+# num = int(input("Enter a number to see if it's odd or even: "))
+
+# if (num % 2) == 0 :
+#     print(f"{num} is even")
+# else :
+#     print(f"{num} is odd.")
+    
+# Exercise 8 : What’s Your Name ?
+
+# my_name = "Jackie"
+# user_name = input("What is your name? ")
+
+# if user_name == my_name :
+#     print("We have the same name!")
+# elif user_name != my_name :
+#     print("We don't have the same name!")
+    
+
+# Exercise 9 : Tall Enough To Ride A Roller Coaster
+
+height = int(input("How tall are you? "))
+
+if height > 145 :
+    print("You can ride.")
+else :
+    print("You can't ride.")
---
+++ Week_2/day1/variables.py
@@ -0,0 +1,69 @@
+# print(2+3) #printing an operation 
+
+#int, float, string, boolean (true / false)
+
+
+# creating a variable - point is to be reused 
+#username = "John"
+
+
+#email = "jackie@gmail.com"
+#position_of_at = email.find("@")
+#print(position_of_at)
+
+#user_name = input("What is your name?")
+#print(user_name)
+
+# user_age = input("what is your age?")
+# print (user_age)
+# print(type(user_age))
+
+#exercise 1
+
+# my_age = 26
+# years = 123879
+
+# print(my_age + years)
+
+# exercise 2
+
+# miles = int(input("Miles to KM converter: Enter number for miles: "))
+# conversion_factor = int(miles / 0.62137119)
+
+# print(f"{miles} miles is equal to {conversion_factor} kilometers")
+
+#exercise 3
+
+# first_name = "Jackie"
+# last_name = "Prezant"
+
+# print(f"My name is {first_name} {last_name}")
+
+
+# Conditionals:
+
+bank_amount = 2500
+computer_price = 5000
+phone_price = 2000
+
+if bank_amount >= computer_price :
+    print("I will be the computer")
+elif bank_amount >= phone_price :
+    print("I will buy the phone")
+else :
+    print("You don't have enough")    
+# if both are true, it will print the first if statement. if neither are true, you will see the else statement
+
+
+Here is a table of prices for a wedding catering company:
+
+# of guests	price
+Up to 50 people	$4,000
+Up to 100 people	$10,000
+Up to 200 people	$15,000
+More than 200 people	$20,000
+
+📝 Instructions:
+
+Please write an program that prompts the user for the number of people attending their wedding and prints the corresponding price in the console.
+For example, if the user says that 20 people are attending to the wedding, it must cost $4,000 dollars.
