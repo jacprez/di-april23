1052a9e4552926319c9837c65a0ac1b129f29de1
jacprez <jprezant1@gmail.com>
2023-05-08 22:05:59 +0300

    week 2 day 2 exercise xp and some of daily challenge

---
+++ Week_2/day2/DailyChallenge/dailychallenge.py
@@ -0,0 +1,20 @@
+# Challenge 1
+
+# number = int(input("Please write a number: "))
+# length = int(input("Please write a number for length: "))
+
+# multiples = []
+# multiply = 1
+
+# while len(multiples) <= length :
+#     multiples.append(number * multiply)
+#     multiply += 1
+
+    
+
+# Challenge 2
+
+users_word = input("Please enter a word: ")
+
+# for letter in users_word :
+#     if 
