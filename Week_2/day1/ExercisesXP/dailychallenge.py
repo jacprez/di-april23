530896732bfec9ad1f39db8d372572a347044330
jacprez <jprezant1@gmail.com>
2023-05-08 09:58:03 +0300

    missing exercise 2 in daily challenge

--- Week_2/day1/ExercisesXP/dailychallenge.py
+++ Week_2/day1/ExercisesXP/dailychallenge.py
@@ -8,7 +8,12 @@ if len(user_input) < 10 :
 elif len(user_input) > 10 :
     print(f"{user_input} is too long")
     user_input = input("Write a word that is 10 characters long: ")
-    
+
+# Exercise 2
+
+user_answer = input("Write a word that is 10 characters long: ")
+
+print(user_answer[0], user_answer[-1])
 
 # Exercise 3
 
