d07e2931e1a94f3d018177524721f1b6227084f1
jacprez <jprezant1@gmail.com>
2023-05-15 10:19:48 +0300

    finish daily challenge

--- Week_3/day1/DailyChallenge/dailychallenge.py
+++ Week_3/day1/DailyChallenge/dailychallenge.py
@@ -20,12 +20,21 @@ class Farm :
     def get_animal_types(self) :
         all_keys = list(self.all_animals.keys())
         return sorted(all_keys)
+    
+    def get_short_info(self):
+        all_animal_keys = self.get_animal_types()
+        for animal, amount in self.all_animals.items() :
+            if amount > 1 :
+                position_animal = all_animal_keys.index(animal)
+                all_animal_keys[position_animal] += 's'
+        joining_animals = ", ".join(all_animal_keys[:-1])
+        sentence = f"\n{self.farm_name} Farm has {joining_animals} and {all_animal_keys[-1]}"
+        print(all_animal_keys)
         
 macdonald = Farm("McDonald")
 macdonald.add_animal('cow',5)
 macdonald.add_animal('sheep')
-macdonald.add_animal('sheep')
 macdonald.add_animal('goat', 12)
 
 print(macdonald.get_animal_types())
-macdonald.get_info()
+macdonald.get_short_info()
