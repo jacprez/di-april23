4bf717e2ba6cdee6b6ea369665bd631bddaa5577
jacprez <jprezant1@gmail.com>
2023-05-17 17:12:13 +0300

    daily challenge fixes

--- Week_3/day4/DailyChallenge/dailychallenge.py
+++ Week_3/day4/DailyChallenge/dailychallenge.py
@@ -1,8 +1,10 @@
 
+
+# Part 1
 class Text:
     def __init__(self, text):
         self.text = text
-
+    # a method to return the frequency of a word in the text
     def word_frequency(self, word):
         words = self.text.split()
         count = words.count(word)
@@ -10,7 +12,7 @@ class Text:
             return f"The word '{word}' does not appear in the text."
         else:
             return f"The word '{word}' appears {count} time(s) in the text."
-
+    # a method that returns the most common word in the text
     def most_common_word(self):
         words = self.text.split()
         word_counts = {}
@@ -20,7 +22,7 @@ class Text:
             return "The text is empty."
         most_common = max(word_counts, key=word_counts.get)
         return f"The most common word in the text is '{most_common}'."
-
+    # a method that returns a list of all the unique words in the text
     def unique_words(self):
         words = self.text.split()
         unique_words = list(set(words))
@@ -33,3 +35,5 @@ my_text = Text(text)
 print(my_text.word_frequency("good"))
 print(my_text.most_common_word())
 print(my_text.unique_words())
+
+# Part 2 - I tried to figure out but I wasn't able to
