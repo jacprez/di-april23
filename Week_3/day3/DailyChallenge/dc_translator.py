b8fbde23b3118a454a39496e83ae636749b31d47
jacprez <jprezant1@gmail.com>
2023-05-18 10:04:24 +0300

    fix week 3 day 3 daily challenge

--- Week_3/day3/DailyChallenge/dc_translator.py
+++ Week_3/day3/DailyChallenge/dc_translator.py
@@ -6,14 +6,14 @@
 # You have to recreate the result using a translator module.
 
 
-from deep_translator import GoogleTranslator
-french_words= ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
+# from deep_translator import GoogleTranslator
+# french_words= ["Bonjour", "Au revoir", "Bienvenue", "A bientôt"]
 
-for word in french_words:
-    english_translation = GoogleTranslator(source='french', target='english').translate(word)
-    translation_dict = {}
-    translation_dict[word] = english_translation
-    print(translation_dict)
+# for word in french_words:
+#     english_translation = GoogleTranslator(source='french', target='english').translate(word)
+#     translation_dict = {}
+#     translation_dict[word] = english_translation
+#     print(translation_dict)
 
 # example = GoogleTranslator(source='auto', target='fr').translate(french_words) 
 # print(example)
@@ -23,3 +23,18 @@ for word in french_words:
 # print(translated_word)
 
 # can't finish because I am having trouble with the modules
+
+# Trying again but with a file
+
+# from translate import Translator
+
+# translator = Translator(to_lang='ja')
+
+# try:
+#     with open('Week_3/day3/DailyChallenge/translation.txt', 'r') as my_file:
+#         text = my_file.read()
+#         translation = translator.translate(text)
+# except FileNotFoundError as e:
+#     print('Check your file path')
+
+# still doesnt work
