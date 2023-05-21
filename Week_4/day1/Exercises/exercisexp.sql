6fc33b31748c345af273a11f0e500564a90c5b86
jacprez <jprezant1@gmail.com>
2023-05-21 16:32:36 +0300

    week 4 day 1 xp

---
+++ Week_4/day1/Exercises/exercisexp.sql
@@ -0,0 +1,39 @@
+-- -- CREATE TABLE items (
+-- 	item_id SERIAL PRIMARY KEY,
+-- 	item_name VARCHAR(100) NOT NULL,
+-- 	item_price INTEGER NOT NULL
+	
+-- )
+-- SELECT * FROM items
+
+-- CREATE TABLE customers (
+-- 	customer_id SERIAL PRIMARY KEY,
+-- 	 first_name VARCHAR (50) NOT NULL,
+-- 	 last_name VARCHAR (100) NOT NULL
+-- )
+
+-- INSERT INTO items (item_name, item_price)
+-- VALUES ('Small Desk', 100), ('Large Desk', 300), ('Fan', 80);
+
+-- SELECT * FROM items
+
+-- INSERT INTO customers (first_name, last_name)
+-- VALUES ('Greg', 'Jones'), ('Sandra', 'Jones'), ('Scott', 'Scott'),('Trevor', 'Green'), ('Melanie', 'Johnson');
+
+-- SELECT * FROM customers
+
+-- SELECT item_name FROM items
+-- WHERE item_price > 80
+
+-- SELECT item_name FROM items
+-- WHERE item_price < 300
+
+
+-- SELECT * FROM customers
+-- WHERE last_name = 'Smith';
+
+-- SELECT * FROM customers
+-- WHERE last_name = 'Jones'
+
+-- SELECT * FROM customers
+-- WHERE first_name != 'Scott'
